40cae26a318131a90650af76f1847f057047d27e
ZobovaA <75171940+ZobovaA@users.noreply.github.com>
2021-05-07 17:17:23 +0300

    pics added

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
-import galleryItems from './gallery-items.js'
-console.log(galleryItems)
-const listEl = document.querySelector('.js-gallery');
+import galleryItems from "./gallery-items.js";
 
-function createEl(items){
-const addImg = galleryItems.map(({ preview, original, description }, index) => {
-    return `
-    <li class="gallery__item">
-<a
-class="gallery__link"
-href="${original}"
->
-<img
-  class="gallery__image"
-  src="${preview}"
-  data-source="${original}"
-  data-index=${index}
-  alt="${description}"
-/>
-</a>
-</li>
-`;
-
-
+const elementsUl = document.querySelector('.js-gallery');
+const addGalleryElements = galleryItems
+    .map(({ preview, original, description }, index) => {
+        return `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      data-index=${index}
+      alt="${description}"
+    />
+  </a>
+</li>`;
 })
-console.log(addImg[0])
 .join(' ');
-}
 
-// listEl.insertAdjacentHTML('beforeend', addImg);
-
-
-
+elementsUl.insertAdjacentHTML('afterbegin', addGalleryElements);
+   
