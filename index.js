2c6b3e1fe7dbd15f1f00702659f354d8373f053a
ZobovaA <75171940+ZobovaA@users.noreply.github.com>
2021-05-08 20:32:48 +0300

    8 js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import galleryItems from "./gallery-items.js";
+import galleryItems from './gallery-items.js'
 
-const elementsUl = document.querySelector('.js-gallery');
-const addGalleryElements = galleryItems
-    .map(({ preview, original, description }, index) => {
-        return `<li class="gallery__item">
-  <a
-    class="gallery__link"
-    href="${original}"
-  >
+const listEl = document.querySelector('.js-gallery')
+const lboxEl = document.querySelector('.js-lightbox')
+const lightboxOverlayEl = document.querySelector('.lightbox__overlay')
+const lightboxImageEl = document.querySelector('.lightbox__image')
+const buttonCloseEl = document.querySelector('.lightbox__button')
+
+const addGalleryElement = galleryItems
+  .map(({ preview, original, description }, index) => {
+    return `<li class="gallery__item">
     <img
       class="gallery__image"
       src="${preview}"
@@ -15,10 +16,24 @@ const addGalleryElements = galleryItems
       data-index=${index}
       alt="${description}"
     />
-  </a>
-</li>`;
-})
-.join(' ');
+ </li>`
+  })
+  .join(' ')
+//1
+listEl.insertAdjacentHTML('beforeend', addGalleryElement)
+//2
+listEl.addEventListener('click', clicker)
+function clicker(event) {
+  if(event.target.src){
+    lboxEl.classList.add('is-open')
+    console.log(event.target.src)
+    lightboxImageEl.src = event.target.src
+  }
 
-elementsUl.insertAdjacentHTML('afterbegin', addGalleryElements);
-   
+}
+
+function closePopup() {
+  lboxEl.classList.remove('is-open') 
+  lightboxImageEl.src = ''
+}
+buttonCloseEl.addEventListener('click', closePopup)
